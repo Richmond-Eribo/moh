c988ca65ba21c35a337a174eb6367bcd4908021c
Richmond Eribo <uyiosaeribo344@gmail.com>
2022-07-02 22:43:29 +0100

    add alt to about

--- pages/AboutUs.tsx
+++ pages/AboutUs.tsx
@@ -1,18 +1,23 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 const AboutUs = () => {
   return (
-    <div className="">
-      <div className="bg-[#005410]">
-        <h1 className="text-[#fff] font-semibold text-3xl uppercase py-8 px-8">
+    <div className=''>
+      <div className='bg-[#005410]'>
+        <h1 className='text-[#fff] font-semibold text-3xl uppercase py-8 px-8'>
           about us
         </h1>
       </div>
-      <div className="">
-        <Image src="/Images/image 1.jpg" height={775} width={1440} />
+      <div className=''>
+        <Image
+          src='/Images/image 1.jpg'
+          height={775}
+          width={1440}
+          alt='about'
+        />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default AboutUs;
+export default AboutUs
