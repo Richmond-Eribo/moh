3369ec8c5b0024c3dd0cb22457922ba4e19534a9
Richmond Eribo <uyiosaeribo344@gmail.com>
2022-07-22 15:24:00 +0100

    add collapse Nav

---
+++ components/Navigation/CollapseNav.tsx
@@ -0,0 +1,44 @@
+import Link from 'next/link'
+
+type Props = {
+  title: string
+  items?: string[]
+  url: string
+}
+
+const CollapseNav = ({title, items, url}: Props) => {
+  return (
+    <div>
+      {/* checks if items is true */}
+      {!items ? (
+        // <Link href={url}>
+        <div className='collapse'>
+          <a className='collapse-title cursor-pointer text-xl my-4 font-semibold hover:text-[#005410] duration-150 ease-in-out'>
+            {title}
+          </a>
+        </div>
+      ) : (
+        // </Link>
+        <div
+          tabIndex={0}
+          className='collapse collapse-arrow  bg-base-100 rounded-box'
+        >
+          {/* <Link href={url}> */}
+          <a className='collapse-title cursor-pointer text-xl mb-4 font-semibold hover:text-[#005410] duration-150 ease-in-out'>
+            {title}
+          </a>
+          {/* </Link> */}
+          <ol className='collapse-content'>
+            {items.map(item => (
+              <li className='py-2 border-b-2' key={item}>
+                {item}
+              </li>
+            ))}
+          </ol>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default CollapseNav
--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -1,12 +1,13 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from 'next/image'
+import Link from 'next/link'
 
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
-import React, { useState } from 'react';
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
+import React, {useState} from 'react'
+import CollapseNav from './CollapseNav'
 const Navigation = function () {
-  const [status, setStatus] = useState(false);
+  const [status, setStatus] = useState(false)
   function handleChange() {
-    setStatus(!status);
+    setStatus(!status)
   }
 
   return (
@@ -24,13 +25,13 @@ const Navigation = function () {
           </Link>
           <ul className='hidden lg:flex  items-center py-5 gap-x-7 justify-between text-lg'>
             {[
-              { name: 'home', url: '/' },
+              {name: 'home', url: '/'},
 
-              { name: 'news', url: '/news' },
-              { name: 'about', url: '/AboutUs' },
-            ].map((nav) => (
+              {name: 'news', url: '/news'},
+              {name: 'about', url: '/AboutUs'},
+            ].map(nav => (
               <Link key={nav.name} href={nav.url}>
-                <a className='font-bold capitalize hover:text-[#005410] duration-150 ease-in-out'>
+                <a className='font-semibold capitalize hover:text-[#005410] duration-150 ease-in-out'>
                   {nav.name}
                 </a>
               </Link>
@@ -54,7 +55,7 @@ const Navigation = function () {
         </span>
 
         {/* large screen navigation */}
-        <ul className='hidden lg:flex  items-center py-5 justify-between text-lg'>
+        <ul className='hidden lg:flex border-t-2 border-b-2 mt-5  items-center py-5 justify-between text-lg'>
           {[
             // { name: 'home', url: '/' },
             {
@@ -80,32 +81,32 @@ const Navigation = function () {
               item3: 'Sickle cell centre',
               item4: ' Integrated services',
             },
-            { name: 'health insurance', url: '/primaryHealthCare' },
-            { name: 'traditional medicine', url: '/traditionalMedicine' },
-          ].map((nav) => (
+            {name: 'health insurance', url: '/primaryHealthCare'},
+            {name: 'traditional medicine', url: '/traditionalMedicine'},
+          ].map(nav => (
             <Link key={nav.name} href={nav.url}>
-              <div className='dropdown dropdown-hover'>
+              <div className='dropdown dropdown-hover '>
                 <a
-                  tabindex='0'
-                  className='cursor-pointer capitalize  font-bold hover:text-[#005410] duration-150 ease-in-out'
+                  tabIndex={0}
+                  className='cursor-pointer capitalize  font-semibold hover:text-[#005410] duration-150 ease-in-out'
                 >
                   {nav.name}
                 </a>
                 <ul
-                  tabindex='0'
-                  class='dropdown-content menu p-2 shadow bg-base-100 rounded-sm w-44'
+                  tabIndex={0}
+                  className='dropdown-content text-sm menu p-2 shadow bg-base-100 rounded-sm w-44'
                 >
                   <li>
-                    <a>{nav.item && nav.item}</a>
+                    <a>{nav.item}</a>
                   </li>
                   <li>
-                    <a>{nav.item2 && nav.item2}</a>
+                    <a>{nav.item2}</a>
                   </li>
                   <li>
-                    <a>{nav.item3 && nav.item3}</a>
+                    <a>{nav.item3}</a>
                   </li>
                   <li>
-                    <a>{nav.item4 && nav.item4}</a>
+                    <a>{nav.item4}</a>
                   </li>
                 </ul>
               </div>
@@ -118,32 +119,52 @@ const Navigation = function () {
       {
         <ul
           className={`lg:hidden duration-100 ease-in-out mb-2 z-50 ${
-            status ? 'h-76 ' : 'invisible h-0 '
+            status ? 'h-screen ' : 'invisible h-0 '
           } container flex flex-col capitalize  gap-x-7`}
         >
           {[
-            { name: 'ministry of health', url: '/ministryOfHealth' },
-            { name: 'primary health care', url: '/primaryHealthCare' },
-            { name: 'specialist care', url: '/specialistCare' },
-            { name: 'health insurance', url: '/primaryHealthCare' },
-            { name: 'traditional medicine', url: '/traditionalMedicine' },
-            { name: 'news', url: '/news' },
-            { name: 'about', url: '/AboutUs' },
-          ].map((nav) => (
-            <Link key={nav.name} href={nav.url}>
-              <a
-                className={`cursor-pointer text-xl mb-4 font-semibold hover:text-[#005410] ${
-                  status ? '' : 'hidden'
-                } duration-150 ease-in-out`}
-              >
-                {nav.name}
-              </a>
-            </Link>
+            {
+              name: 'ministry of health',
+              url: '/ministryOfHealth',
+              items: [
+                'Public health',
+                'Regulation and monitoring',
+                'Health planning, policy, research and statistics',
+              ],
+            },
+            {
+              name: 'primary health care',
+              url: '/primaryHealthCare',
+              items: [
+                'Diseases Control and Immunization',
+                'Community and Family Health Services',
+                'Planning, monitoring and evaluation',
+              ],
+            },
+
+            {
+              name: 'specialist care',
+              url: '/specialistCare',
+              items: [
+                'Hospitals ',
+                'Essential Drugs Programme',
+                'Sickle cell centre',
+                ' Integrated services',
+              ],
+            },
+            {name: 'health insurance', url: '/primaryHealthCare'},
+            {name: 'traditional medicine', url: '/traditionalMedicine'},
+            {name: 'news', url: '/news'},
+            {name: 'about', url: '/AboutUs'},
+          ].map(nav => (
+            <div key={nav.name} className={` ${status ? '' : 'hidden'} `}>
+              <CollapseNav title={nav.name} url={nav.url} items={nav.items} />
+            </div>
           ))}
         </ul>
       }
     </section>
-  );
-};
+  )
+}
 
-export default Navigation;
+export default Navigation
