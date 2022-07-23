276d5fdfc7cfbdd4b432095998f91ba264731bb2
Oshoke <oyatioshoke@yahoo.com>
2022-07-23 23:16:21 +0100

    shokes

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -2,39 +2,15 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { IoMdRadioButtonOff } from 'react-icons/io';
 import { Fade } from 'react-awesome-reveal';
+import SliderLarge from 'components/SliderLarge';
+import SliderSmall from 'components/SliderSmall';
 
 const Hero = function () {
-  const [heroImageNum, setHeroImageNum] = useState(1);
-
-  const [heroImage, setHeroImage] = useState('/Images/heroImage.png');
-
-  const intervalId = setInterval(() => {
-    // console.log('cleariing');
-    if (heroImageNum === 1) {
-      setHeroImageNum(heroImageNum + 1);
-    } else if (heroImageNum === 2) {
-      setHeroImageNum(heroImageNum + 1);
-    } else if (heroImageNum === 3) {
-      setHeroImageNum(1);
-    }
-    clearInterval(intervalId);
-  }, 5000);
-
-  useEffect(() => {
-    if (heroImageNum === 1) {
-      setHeroImage('/Images/heroImage.png');
-    } else if (heroImageNum === 2) {
-      setHeroImage('/Images/info - 005.png');
-    } else if (heroImageNum === 3) {
-      setHeroImage('/Images/info - 006.png');
-    }
-  }, [heroImageNum]);
-
   return (
     <section className=' lg:block mb-10  lg:mt-0 lg:mb-0 px-4'>
-      <div className='flex flex-col text-center lg:text-left lg:flex-row items-center  gap-x-7 lg:pl-[5rem]'>
+      <div className='flex flex-col text-center lg:text-left lg:flex-row items-center  gap-x-[3rem] lg:pl-[5rem]'>
         {/* for laptop */}
-        <div className='lg:w-[36rem] hidden lg:block w-full'>
+        <div className='lg:w-[60rem] hidden lg:block w-full'>
           <h3 className='font-bold capitalize text-4xl text-black mb-4'>
             edo state ministry of health
           </h3>
@@ -51,77 +27,12 @@ const Hero = function () {
             read more
           </button>
         </div>
-
-        <div className='relative -translate-y-4 lg:translate-y-0 '>
-          <div className=' w-[842px] hidden lg:block h-screen relative'>
-            <Image src={heroImage} layout='fill' alt='hero image' />
-
-            {heroImageNum === 1 ? (
-              <div className='absolute bottom-[40px] left-[40px] text-white'>
-                <h3 className='font-bold text-xl capitalize'>quality </h3>
-                <h3 className='font-bold text-xl capitalize'>healthcare</h3>
-                <p className='w-[24rem]'>
-                  The new design of our healthcare system emphasizes the primary
-                  healthcare system so that you can have healthcare services not
-                  more than five kilometers from where you live
-                </p>
-              </div>
-            ) : heroImageNum === 2 ? (
-              <div className='absolute bottom-[40px] left-[40px] text-white'>
-                <h3 className='font-bold text-xl capitalize'> </h3>
-                <h3 className='font-bold text-xl capitalize'>healthcare</h3>
-                <p className='w-[24rem]'>
-                  The new design of our healthcare system emphasizes the primary
-                  healthcare system so that you can have healthcare services not
-                  more than five kilometers from where you live
-                </p>
-              </div>
-            ) : null}
-            <div className='absolute right-1/2 translate-x-1 flex items-center gap-2 bottom-5'>
-              <span>
-                {' '}
-                <IoMdRadioButtonOff
-                  className={`cursor-pointer w-3 h-3 rounded-full ${
-                    heroImageNum === 1 ? 'bg-green-700' : null
-                  }`}
-                />
-              </span>{' '}
-              <span>
-                {' '}
-                <IoMdRadioButtonOff
-                  className={`cursor-pointer w-3 h-3 rounded-full ${
-                    heroImageNum === 2 ? 'bg-green-700' : null
-                  }`}
-                />
-              </span>{' '}
-              <span>
-                {' '}
-                <IoMdRadioButtonOff
-                  className={`cursor-pointer  w-3 h-3 rounded-full ${
-                    heroImageNum === 3 ? 'bg-green-700' : null
-                  }`}
-                />
-              </span>
-            </div>
-          </div>
-
-          {/* <div className='w-screen lg:hidden h-[585px] '>
-            <Image src='/Images/heroImage.png' layout='fill' alt='heroimage' />
-          </div> */}
-
-          {/* <div className='absolute hidden bottom-[40px] left-[40px] text-white'>
-            <h3 className='font-bold text-xl capitalize'>quality </h3>
-            <h3 className='font-bold text-xl capitalize'>healthcare</h3>
-            <p className='w-[24rem]'>
-              The new design of our healthcare system emphasizes the primary
-              healthcare system so that you can have healthcare services not
-              more than five kilometers from where you live
-            </p>
-          </div> */}
-        </div>
-
+        <SliderLarge />
         {/* for mobile */}
         <div className='lg:w-[36rem] lg:hidden w-full'>
+          <div className='lg:hidden mb-2'>
+            <SliderSmall />
+          </div>
           <h3 className='font-bold capitalize text-4xl text-black mb-4'>
             edo state ministry of health
           </h3>
---
+++ components/SliderSmall/index.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { IoMdRadioButtonOff } from 'react-icons/io';
+import Image from 'next/image';
+import { useEffect, useState, useRef } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/effect-fade';
+import { Autoplay, EffectFade, Pagination, Navigation } from 'swiper';
+
+const SliderSmall = () => {
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        effect={'fade'}
+        centeredSlides={true}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Autoplay, EffectFade, Pagination]}
+        className='mySwiper'
+      >
+        <SwiperSlide>
+          <div>
+            <img
+              src='/Images/heroImage.png'
+              className='  h-screen'
+              alt='hero image 1'
+            />
+            <div className='absolute bottom-[40px] left-[40px] text-white'>
+              <h3 className='font-bold text-xl capitalize'>quality </h3>
+              <h3 className='font-bold text-xl capitalize'>healthcare</h3>{' '}
+              <p className='w-[24rem]'>
+                // The new design of our healthcare system emphasizes the
+                primary // healthcare system so that you can have healthcare
+                services not // more than five kilometers from where you live //{' '}
+              </p>{' '}
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {' '}
+          <div>
+            <img
+              src='/Images/info - 005.png'
+              className='  h-screen'
+              alt='hero image 2'
+            />
+            <div className='absolute bottom-[40px] left-[40px] text-white'>
+              <h3 className='font-bold text-xl capitalize'>quality </h3>
+              <h3 className='font-bold text-xl capitalize'>healthcare</h3>{' '}
+              <p className='w-[24rem]'>
+                // The new design of our healthcare system emphasizes the
+                primary // healthcare system so that you can have healthcare
+                services not // more than five kilometers from where you live //{' '}
+              </p>{' '}
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <img
+              src='/Images/info - 006.png'
+              className=' h-screen'
+              alt='hero image 2'
+            />
+            <div className='absolute bottom-[40px] left-[40px] text-white'>
+              <h3 className='font-bold text-xl capitalize'>quality </h3>
+              <h3 className='font-bold text-xl capitalize'>healthcare</h3>{' '}
+              <p className='w-[24rem]'>
+                // The new design of our healthcare system emphasizes the
+                primary // healthcare system so that you can have healthcare
+                services not // more than five kilometers from where you live //{' '}
+              </p>{' '}
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+export default SliderSmall;
