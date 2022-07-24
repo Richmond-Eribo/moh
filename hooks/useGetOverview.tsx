e8a8d88c875d34bc0007727bd35bc99c273e055c
Richmond Eribo <uyiosaeribo344@gmail.com>
2022-07-25 00:30:09 +0100

    Add react query hook

---
+++ hooks/useGetOverview.tsx
@@ -0,0 +1,17 @@
+import {client} from 'utils/contentful'
+import {EntryCollection, Entry, Asset} from 'contentful'
+// import {Query} from 'react-query'
+import {useQuery} from 'react-query'
+
+import {OverviewField} from 'interfaces/contentfulTypes'
+
+async function getOverview() {
+  const {items}: EntryCollection<OverviewField> = await client.getEntries({
+    content_type: 'overview',
+  })
+  return items
+}
+
+export default function useGetOverview() {
+  return useQuery('overview', getOverview)
+}
