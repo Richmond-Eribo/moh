88dbabcde8d2c21d2467f4ebc2e169bf08172274
Richmond Eribo <uyiosaeribo344@gmail.com>
2022-07-29 16:18:15 +0100

    add dynamic news page

--- hooks/useGetNews.tsx
+++ hooks/useGetNews.tsx
@@ -5,15 +5,28 @@ import {EntryCollection, Entry, Asset} from 'contentful'
 import {useQuery} from 'react-query'
 import {NewsField} from 'interfaces/contentfulTypes'
 
+type Slug = string | string[]
+
 // the news are fetch from contentful with the client.getEntries
-async function getNews() {
+export async function getNews(skipQuery: number, limit: number) {
   const {items}: EntryCollection<NewsField> = await client.getEntries({
     content_type: 'blog',
+    limit: limit,
+    skip: skipQuery,
   })
   return items
 }
 
-// The hook
-export default function useGetNews() {
-  return useQuery('News', getNews)
+// Get single News for the News Page.
+export async function getSingleNews(slug: Slug) {
+  const {items}: EntryCollection<NewsField> = await client.getEntries({
+    content_type: 'blog',
+    'fields.slug': slug,
+  })
+  return items[0]
+}
+
+// The hook to get all news post with useQuery
+export default function useGetNews(skipQuery = 0, limit = 8) {
+  return useQuery('News', () => getNews(skipQuery, limit))
 }
--- pages/SingleNews/[slug].tsx
+++ pages/SingleNews/[slug].tsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import {getNews, getSingleNews} from 'hooks/useGetNews'
+import {GetStaticPaths, GetStaticProps} from 'next'
+import React from 'react'
 
-const SingleNews = () => {
+type Props = {
+  [key: string]: any
+}
+
+const SingleNews = ({News}: Props) => {
   return (
     <section>
+      {/* this is a single news post */}
+      <>{console.log('single news post:', News)}</>
+
       <div className='bg-[#005410]'>
         <h1 className='text-[#fff] font-semibold text-3xl uppercase py-8 container'>
           News titlte
         </h1>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default SingleNews;
+export default SingleNews
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const News = await getNews(0, 1000)
+
+  const paths = News.map(news => {
+    return {
+      params: {
+        slug: news.fields.slug,
+      },
+    }
+  })
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({params}) => {
+  const News = await getSingleNews(params!.slug!)
+
+  return {
+    props: {
+      News: News.fields,
+    },
+  }
+}
