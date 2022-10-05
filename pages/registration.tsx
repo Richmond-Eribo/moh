7f44af34ea2eabfc156bc3615e2f0e2edf76928d
Richmond Eribo <uyiosaeribo344@gmail.com>
2022-10-05 12:14:12 +0100

    Update registration page 1

--- pages/registration.tsx
+++ pages/registration.tsx
@@ -18,50 +18,53 @@ const Registration = () => {
           ClassNames='text-[#fff] font-semibold text-3xl uppercase py-8 container'
         />
       </div>
-      <main className='flex flex-col items-center'>
-        <h1 className=' font-semibold text-3xl uppercase py-8 '>
-          Steps to follow for a PHC registration
-        </h1>
+      <main className='flex flex-col lg:flex-row justify-between  bg- container'>
+        <div>
+          <h1 className=' font-semibold text-xl uppercase  '>
+            Steps to follow for a PHC registration
+          </h1>
 
-        <ul className='steps steps-vertical'>
-          <li className='step step-primary'>
-            Application letter to the honourable commissioner for health
-          </li>
-          <li className='step step-primary'>
-            Non refundable fee of N10,000 application fee payable to Edo State
-            IGR account
-          </li>
-          <li className='step'>
-            Application fee must be returned within 90days of collection with
-            the following documents attached;
-          </li>
-          {/* <li className='step'>Receive Product</li> */}
-        </ul>
-
-        <h1 className='font-semibold text-xl uppercase py-8 '>Documents </h1>
-        <ol className='list-disc lg:w-[600px] md:w-[70%] w-11/12 mb-12'>
-          <li>
-            Certificate of full registration, certificate of annual practicing
-            licence, degree certificate and other relevant credentials as
-            applicable to staff
-          </li>
-          <li>2 passport photograph</li>
-          <li>Newspaper change of name for married females</li>
-          <li>
-            Tax clearance certificate (3 years tax clearance prior to the
-            current year or tax identification number certificate)
-          </li>
-          <li>Building layout of the facility (4 copies)</li>
-          <li>Building plan of the facility (4 copies</li>
-          <li>List of equipment</li>
-          <li>Direction to the facility (4 copies)</li>
-          <li>CAC certificate</li>
-          <li>
-            A letter of commitment to share patients attendance, morbidity,
-            mortality statistics and any other relevant data with the ministry
-            of health on a monthly basis.
-          </li>
-        </ol>
+          <ul className='steps steps-vertical shadow-xl lg:w-[500px] p-5 rounded-[20px]'>
+            <li className='step   text-xl capitalize '>
+              Read the guidelines for registration
+              {/* <Link href='/'>
+              <a>here</a>
+            </Link>
+            to read guidelines */}
+            </li>
+            <li className='step   text-xl capitalize'>
+              Complete registration form
+            </li>
+            <li className='step   text-xl capitalize '>Upload documents</li>
+            {/* <li className='step'>Receive Product</li> */}
+          </ul>
+        </div>
+        <div>
+          <h1 className='font-semibold text-xl uppercase  '>Documents </h1>
+          <ol className='list-disc shadow-xl  p-10 rounded-[20px] text-xl lg:w-[600px] md:w-[70%] w-11/12 mb-12'>
+            <li>
+              Certificate of full registration, certificate of annual practicing
+              licence, degree certificate and other relevant credentials as
+              applicable to staff
+            </li>
+            <li>2 passport photograph</li>
+            <li>Newspaper change of name for married females</li>
+            <li>
+              Tax clearance certificate (3 years tax clearance prior to the
+              current year or tax identification number certificate)
+            </li>
+            <li>Building layout of the facility (4 copies)</li>
+            <li>Building plan of the facility (4 copies</li>
+            <li>List of equipment</li>
+            <li>Direction to the facility (4 copies)</li>
+            <li>CAC certificate</li>
+            <li>
+              A letter of commitment to share patients attendance, morbidity,
+              mortality statistics and any other relevant data with the ministry
+              of health on a monthly basis.
+            </li>
+          </ol>
+        </div>
       </main>
     </section>
   )
